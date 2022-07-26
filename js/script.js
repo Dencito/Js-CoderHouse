8191bd4ab8b30b6df12691ca6da9d1511e750847
Denar Padilla <denarpadilla.lionel@gmail.com>
2022-07-25 21:49:20 -0300

    Segunda entrega del trabajo, implementando aplicacion de un Boliche :D

--- js/script.js
+++ js/script.js
@@ -1,131 +1,96 @@
-let opcion = parseInt(prompt('Seleccione una opcion \n 1.-Calcular promedio \n 2.-Ver Tabla de multiplicacion \n 3.-Realizar cuenta aritmetica \n 0.-Salir'));
-while(opcion != 0){
-    opcion = parseInt(prompt('Seleccione una opcion \n 1.-Calcular promedio \n 2.-Ver Tabla de multiplicacion \n 3.-Realizar cuenta aritmetica \n 0.-Salir'));
-    switch(opcion){
-        case 1:
-            alert('Selecciono calcular promedio');
-            let nota1 = parseInt(prompt('Ingrese la primer la nota'));
-            while(nota1 < 1){
-                nota1= parseInt(prompt('Porfavor ingrese un numero positivo'));
-            }
-            let nota2 = parseInt(prompt('Ingrese la segunda la nota'));
-            while(nota2 < 1){
-                nota2= parseInt(prompt('Porfavor ingrese un numero positivo'));
-            }
-            let nota3 = parseInt(prompt('Ingrese la tercera nota'));
-            while(nota3 < 1){
-                nota3= parseInt(prompt('Porfavor ingrese un numero positivo'));
-            }
-            let notas = (nota1 + nota2 + nota3)/3;
-            alert('su promedio es de: '+ (notas));
-            if(notas > 5){
-                alert('Usted a aprobado la materia');
-            }else if(notas < 6){
-                alert('Usted desaprobo la materia');
-            }
-            break;
-    
-        case 2:
-            alert('Selecciono ver tabla');
-            let selTabla = parseInt(prompt('Seleccione la tabla que quiere ver. del 1 al 12'));
-            while(selTabla < 1 || selTabla >12){
-                selTabla= parseInt(prompt('Porfavor ingrese una tabla positiva y menor a 12'));
-            }
-    
-            for (let i = 1; i <=10; i++) {
-                alert(selTabla+' * '+ i + ' = '+ (selTabla*i))
-            }
-            break;
+//ACCESO A UN BOLICHE
 
-        case 3:
-            alert('Selecciono Realizar cuenta aritmetica')
-            let opcionAritmetica = parseInt(prompt('Ingrese que operacion que desea realizar. \n 1.-Suma \n 2.-Resta \n 3.-Multiplicacion \n 4.-Division \n 5.-Potencia \n 6.-Raiz \n 0.-Salir'));
-            switch(opcionAritmetica){
+let ingresarEdad = '';
+do{
+    ingresarEdad = parseInt(prompt('Porfavor ingrese su edad'));
+    if(ingresarEdad < 18 && ingresarEdad > 0){
+        alert('Usted es menor de edad, no esta permitido menores de edad')
+    }else if(ingresarEdad >= 18 && ingresarEdad > 0){
+        alert('Edad permitida, siguiente paso')
+        let ingresarDNI = parseInt(prompt('Ingrese su DNI'));
+        while(ingresarDNI.toString().length != 8){
+            ingresarDNI = '';
+            if(ingresarDNI.toString().length < 9) {
+                ingresarDNI = parseInt(prompt('Ingrese nuevamente su DNI'));
+            }
+        }
+        alert('Puede acceder al boliche DP');
+        let menuBoliche = '';
+        do{
+            menuBoliche = parseInt(prompt('Menu del boliche \n 1.- 4 Botellas de cerveza \n 2.- 2 Botellas de Champagne \n 3.- Fernet 1Lt + Coca 3Lt \n 4.- Vino 1Lt + Coca 2,25Lt \n 5.- Smirnoff + 2 Baggio \n 6.- Ron + Coca 3Lt \n 7.- Otros \n 0.- Salir del menu'))
+            switch(menuBoliche){
                 case 1:{
-                    alert('Selecciono Realizar una Suma');
-                    let ValorA = parseInt(prompt('Ingrese el primer valor'));
-                    let ValorB = parseInt(prompt('Ingrese el segundo valor'));
-                    let resultado = ValorA + ValorB;
-                    alert('El resultado de la suma es: '+resultado);
+                    let cantidad = parseInt(prompt('Precio por el combo: $2000\n Ingrese cuantos desea comprar'));
+                    let precioTotal = (cantidadCombo)=>{
+                        const PRECIOCOMBO= 2000;
+                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de 4 cervezas su total es de: $'+ PRECIOCOMBO*cantidadCombo)
                     }
-                break;
-    
+                    precioTotal(cantidad);//luego agregare condicionales :D
+                    }
+                    break;
                 case 2:{
-                    alert('Selecciono Realizar una Resta');
-                    let ValorA = parseInt(prompt('Ingrese el primer valor'));
-                    let ValorB = parseInt(prompt('Ingrese el segundo valor'));
-                    let resultado = ValorA - ValorB;
-                    alert('El resultado de la Resta es: '+resultado);
+                    let cantidad = parseInt(prompt('Precio por el combo: $1500\n Ingrese cuantos desea comprar'));
+                    let precioTotal = (cantidadCombo)=>{
+                        const PRECIOCOMBO= 1500;
+                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de 2 Botellas de Champagne su total es de: $'+ PRECIOCOMBO*cantidadCombo)
                     }
-                break;
-    
+                    precioTotal(cantidad);//luego agregare condicionales :D
+                    }
+                    break;
                 case 3:{
-                    alert('Selecciono Realizar una Multiplicaion');
-                    let ValorA = parseInt(prompt('Ingrese el primer valor'));
-                    let ValorB = parseInt(prompt('Ingrese el segundo valor'));
-                    let resultado = ValorA * ValorB;
-                    alert('El resultado de la Multiplicaion es: '+resultado);
+                    let cantidad = parseInt(prompt('Precio por el combo: $1800\n Ingrese cuantos desea comprar'));
+                    let precioTotal = (cantidadCombo)=>{
+                        const PRECIOCOMBO= 1800;
+                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de Fernet 1Lt + Coca 3Lt su total es de: $'+ PRECIOCOMBO*cantidadCombo)
                     }
-                break;
-    
+                    precioTotal(cantidad);//luego agregare condicionales :D
+                    }
+                    break;
                 case 4:{
-                    alert('Selecciono Realizar una Division');
-                    let ValorA = parseInt(prompt('Ingrese el primer valor'));
-                    let ValorB = parseInt(prompt('Ingrese el segundo valor'));
-                    let resultado = ValorA / ValorB;
-                    alert('El resultado de la Division es: '+resultado);
+                    let cantidad = parseInt(prompt('Precio por el combo: $800\n Ingrese cuantos desea comprar'));
+                    let precioTotal = (cantidadCombo)=>{
+                        const PRECIOCOMBO= 800;
+                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de Vino 1Lt + Coca 2,25Lt su total es de: $'+ PRECIOCOMBO*cantidadCombo)
                     }
-                break;
-    
+                    precioTotal(cantidad);//luego agregare condicionales :D
+                    }
+                    break;
                 case 5:{
-                    alert('Selecciono Realizar una Potencia');
-                    let base = parseInt(prompt('Ingrese la base'));
-                    let expo = parseInt(prompt('Ingrese el exponente'));
-                    let acu= 1;
-                    
-                    for (let i = 0; i < expo; i++) {
-                        acu= acu * base;
+                    let cantidad = parseInt(prompt('Precio por el combo: $1400\n Ingrese cuantos desea comprar'));
+                    let precioTotal = (cantidadCombo)=>{
+                        const PRECIOCOMBO= 1400;
+                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de Smirnoff + 2 Baggio su total es de: $'+ PRECIOCOMBO*cantidadCombo)
                     }
-                    alert('El resultado de la potencia es: '+ acu);
-    
+                    precioTotal(cantidad);//luego agregare condicionales :D
                     }
-                break;
-    
+                    break;
                 case 6:{
-                    let opcionRaiz= parseInt(prompt('Selecciono Realizar una Raiz \n Porfavor Elija \n 1.-Raiz Cuadrada \n 2.-Raiz Cubica \n 0.-Salir'));
-                    switch(opcionRaiz){
-                        case 1:{
-                            let num = parseInt(prompt('Ingrese la base de la raiz cuadratica'));
-                            while(num < 1){
-                                num= parseInt(prompt('Porfavor ingrese una base positiva'));
-                            }
-                            let raizCuadrada = Math.sqrt(num);
-                            alert('El resultado de su raiz cuadrada es: '+ raizCuadrada);
-                            }
-                            break;
-                        
-                        case 2:{
-                            
-                            let num = parseInt(prompt('Ingrese la base de la raiz cubica'));
-                            while(num < 1){
-                                num= parseInt(prompt('Porfavor ingrese una base positiva'));
-                            }
-                            let raizCubica = Math.pow(num, 1 / 3);
-                            alert('El resultado de su raiz cubica es: '+ raizCubica);
-                            }
-                            }
+                    let cantidad = parseInt(prompt('Precio por el combo: $2400\n Ingrese cuantos desea comprar'));
+                    let precioTotal = (cantidadCombo)=>{
+                        const PRECIOCOMBO= 2400;
+                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de Ron + Coca 3Lt su total es de: $$'+ PRECIOCOMBO*cantidadCombo)
                     }
-            }  
-            break;
-        case 0:
-            
-            break;
-        
-        default:
-            alert('El numero no esta en el menu ingrese otro');
-            break;
-            //shodan
+                    precioTotal(cantidad);//luego agregare condicionales :D
+                    }
+                    break;
+                case 7:{
+                    alert('Luego se agregara mas productos, gracias \n Vuelva pronto...')
+                    }
+                    break;
+                case 0:{
+                    alert('Selecciono salir')
+                    }
+                    break;
+                default:{
+                    alert('Opcion invalida');
+                    break;
+                    }
+            }
+        }while(menuBoliche!=0)
+        alert('Gracias por comprar en Boliche DP');
+
+    }else {
+        alert('Porfavor ingrese un numero mayor a 0')
+    }
     
-    
-    } 
-}alert('Selecciono Salir, Saliendo...');
+}while(ingresarEdad < 1)
