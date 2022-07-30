3587fdcf8f71ccebb3e889686e9f205fcc44db50
Denar Padilla <denarpadilla.lionel@gmail.com>
2022-07-30 16:19:24 -0300

    Agregando array, modificando index.hmtl, cambiando formato de imagenes, agregando menu hamburgesa, animaciones y condicones en JS

--- js/script.js
+++ js/script.js
@@ -17,13 +17,20 @@ do{
         alert('Puede acceder al boliche DP');
         let menuBoliche = '';
         do{
-            menuBoliche = parseInt(prompt('Menu del boliche \n 1.- 4 Botellas de cerveza \n 2.- 2 Botellas de Champagne \n 3.- Fernet 1Lt + Coca 3Lt \n 4.- Vino 1Lt + Coca 2,25Lt \n 5.- Smirnoff + 2 Baggio \n 6.- Ron + Coca 3Lt \n 7.- Otros \n 0.- Salir del menu'))
+            const combos = ['4 Botellas de cerveza','2 Botellas de Champagne','Fernet 1Lt + Coca 3Lt','Vino 1Lt + Coca 2,25Lt','Smirnoff + 2 Baggio','Ron + Coca 3Lt'];
+            const preciosDeCombos = [2000, 1500, 1800, 800, 1400, 2400];
+            menuBoliche = parseInt(prompt('Menu del boliche \n 1.- '+combos[0]+' \n 2.- '+combos[1]+' \n 3.- '+combos[2]+' \n 4.- '+combos[3]+' \n 5.- '+combos[4]+' \n 6.- '+combos[5]+' \n 7.- Otros \n 0.- Salir del menu'))
             switch(menuBoliche){
                 case 1:{
                     let cantidad = parseInt(prompt('Precio por el combo: $2000\n Ingrese cuantos desea comprar'));
                     let precioTotal = (cantidadCombo)=>{
-                        const PRECIOCOMBO= 2000;
-                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de 4 cervezas su total es de: $'+ PRECIOCOMBO*cantidadCombo)
+                        const PRECIOCOMBO= preciosDeCombos[0];
+                        if(cantidad == 1){
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combo de '+combos[0]+' su total es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }else{
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de '+combos[0]+' su total es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }
+                        
                     }
                     precioTotal(cantidad);//luego agregare condicionales :D
                     }
@@ -31,8 +38,12 @@ do{
                 case 2:{
                     let cantidad = parseInt(prompt('Precio por el combo: $1500\n Ingrese cuantos desea comprar'));
                     let precioTotal = (cantidadCombo)=>{
-                        const PRECIOCOMBO= 1500;
-                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de 2 Botellas de Champagne su total es de: $'+ PRECIOCOMBO*cantidadCombo)
+                        const PRECIOCOMBO= preciosDeCombos[1];
+                        if(cantidad == 1){
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combo de '+combos[1]+' es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }else{
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de '+combos[1]+' es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }
                     }
                     precioTotal(cantidad);//luego agregare condicionales :D
                     }
@@ -40,8 +51,12 @@ do{
                 case 3:{
                     let cantidad = parseInt(prompt('Precio por el combo: $1800\n Ingrese cuantos desea comprar'));
                     let precioTotal = (cantidadCombo)=>{
-                        const PRECIOCOMBO= 1800;
-                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de Fernet 1Lt + Coca 3Lt su total es de: $'+ PRECIOCOMBO*cantidadCombo)
+                        const PRECIOCOMBO= preciosDeCombos[2];
+                        if(cantidad == 1){
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combo de '+combos[2]+' es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }else{
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de '+combos[2]+' es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }
                     }
                     precioTotal(cantidad);//luego agregare condicionales :D
                     }
@@ -49,8 +64,12 @@ do{
                 case 4:{
                     let cantidad = parseInt(prompt('Precio por el combo: $800\n Ingrese cuantos desea comprar'));
                     let precioTotal = (cantidadCombo)=>{
-                        const PRECIOCOMBO= 800;
-                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de Vino 1Lt + Coca 2,25Lt su total es de: $'+ PRECIOCOMBO*cantidadCombo)
+                        const PRECIOCOMBO= preciosDeCombos[3];
+                        if(cantidad == 1){
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combo de '+combos[3]+' es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }else{
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de '+combos[3]+' es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }
                     }
                     precioTotal(cantidad);//luego agregare condicionales :D
                     }
@@ -58,8 +77,12 @@ do{
                 case 5:{
                     let cantidad = parseInt(prompt('Precio por el combo: $1400\n Ingrese cuantos desea comprar'));
                     let precioTotal = (cantidadCombo)=>{
-                        const PRECIOCOMBO= 1400;
-                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de Smirnoff + 2 Baggio su total es de: $'+ PRECIOCOMBO*cantidadCombo)
+                        const PRECIOCOMBO= preciosDeCombos[4];
+                        if(cantidad == 1){
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combo de '+combos[4]+' es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }else{
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de '+combos[4]+' es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }
                     }
                     precioTotal(cantidad);//luego agregare condicionales :D
                     }
@@ -67,14 +90,30 @@ do{
                 case 6:{
                     let cantidad = parseInt(prompt('Precio por el combo: $2400\n Ingrese cuantos desea comprar'));
                     let precioTotal = (cantidadCombo)=>{
-                        const PRECIOCOMBO= 2400;
-                        alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de Ron + Coca 3Lt su total es de: $$'+ PRECIOCOMBO*cantidadCombo)
+                        const PRECIOCOMBO= preciosDeCombos[5];
+                        if(cantidad == 1){
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combo de '+combos[5]+' es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }else{
+                            alert('Usted a seleccionado comprar '+ cantidadCombo + ' combos de '+combos[5]+' es de: $'+ PRECIOCOMBO*cantidadCombo);
+                        }
                     }
                     precioTotal(cantidad);//luego agregare condicionales :D
                     }
                     break;
                 case 7:{
                     alert('Luego se agregara mas productos, gracias \n Vuelva pronto...')
+                    let ingresarPropuesta = '';
+                    let ingresarPropuestaPrecio = '';
+                    do{
+                        ingresarPropuesta = prompt('Ingrese el combo de bebidas que le gustaria que este en el menu');
+                        ingresarPropuestaPrecio = parseInt(prompt('Ingrese que precio que le pondria al combo'));
+                    }while(ingresarPropuesta.length < 1 && ingresarPropuestaPrecio.length < 1)
+
+                    combos.push(ingresarPropuesta);
+                    preciosDeCombos.push(ingresarPropuestaPrecio);
+                    console.log('todos los productos \n'+combos.join('\n'));
+                    console.log('todos los precios de los productos \n'+preciosDeCombos.join('\n'));
+                    alert('Propuesta enviada exitosamente')
                     }
                     break;
                 case 0:{
